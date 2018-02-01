282fd7cd34a0b404d30f51530237aa39a9b35df9
Edmund <yurielber@gmail.com>
2018-02-01 11:01:41 +0800

    Update whois.py

--- whois.py
+++ whois.py
@@ -1,18 +1,21 @@
-#!/usr/bin/env python
+# !/usr/bin/env python
 
 import sys
 import telnetlib
 
-whoisServers = ['whois.iana.org', 'whois.verisign-grs.com', 'whois.markmonitor.com' ]
+whoisServers = ['whois.iana.org', 'whois.verisign-grs.com', 'whois.markmonitor.com']
 port = '43'
 timeout = 5
 
 for host in whoisServers:
-    print("Connecting to {}".format( (host,) ))
+    print('Connecting to {}'.format(host))
     tn = telnetlib.Telnet(host, port, timeout)
     tn.write("com" + "\n")
-    print("-----------------------------")
+
     contents = tn.read_all()
+
+    print("-----------------------------")
     print(contents)
     print("-----------------------------")
+
     print("Disconnected from {}".format(host))
