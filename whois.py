e39c546d05f4ee20b0de1f18e801b8ff56b7d863
Edmund <yurielber@gmail.com>
2018-02-01 14:34:57 +0800

    Update whois.py

--- whois.py
+++ whois.py
@@ -2,20 +2,50 @@
 
 import sys
 import telnetlib
+import re
 
-whoisServers = ['whois.iana.org', 'whois.verisign-grs.com', 'whois.markmonitor.com']
+rootWhoisServer = 'whois.iana.org'
 port = '43'
-timeout = 5
 
-for host in whoisServers:
-    print('Connecting to %s' % (host))
+def printLine():
+    print('-' * 80)
+
+def lookup_once(domain, host=None, port='43', timeout=5):
+    if host is None:
+        host = rootWhoisServer
+    printLine()
+    print('Connecting to %s' % (host) )
+    printLine()
+
     tn = telnetlib.Telnet(host, port, timeout)
-    tn.write("com" + "\n")
-
+    tn.write( domain + "\n")
     contents = tn.read_all()
 
-    print("-----------------------------")
     print(contents)
-    print("-----------------------------")
 
-    print("Disconnected from {}".format(host))
+    printLine()
+    print('Disconnected from %s' % (host))
+    printLine()
+
+    return contents
+
+def lookup(domain):
+    feedback = lookup_once(domain)
+
+    refer_matcher = r'^\s*refer:\s*(?P<whoisServer>whois[.].*)$'
+    next_whois_server = None
+    for line in feedback:
+        if (re.match(refer_matcher, line, re.I)):
+            m = re.search(refer_matcher, line, re.I)
+            next_whois_server = m.group('whoisServer')
+            break
+    # do another lookup
+    if next_whois_server is not None:
+        lookup_once(domain, host=next_whois_server)
+
+if __name__ == '__main__':
+    input_params_num = len(sys.argv)
+    if (input_params_num <= 1):
+        raise RuntimeError('Invalid domain')
+    target = str(sys.argv[1])
+    lookup(target)
